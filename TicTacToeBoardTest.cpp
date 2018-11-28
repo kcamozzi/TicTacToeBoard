4fcdda312ded7d2a63ad8c5641c594720b58e641
Kevin <kevincamozzi@gmail.com>
2018-11-28 19:50:49 +0000

    Unit testing outline

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -20,3 +20,31 @@ TEST(TicTacToeBoardTest, unitTestName)
 	ASSERT_TRUE(true);
 }
 */
+
+TEST(TicTacToeBoardTest, toggleTurn)
+{
+	TicTacToeBoard obj;
+	Piece outcome = obj.toggleTurn();
+	ASSERT_EQ(outcome, Invalid);	
+}
+
+TEST(TicTacToeBoardTest, placePiece)
+{
+	TicTacToeBoard obj;
+	Piece outcome = obj.placePiece(0,0);
+	ASSERT_EQ(outcome, Invalid);	
+}
+
+TEST(TicTacToeBoardTest, getPiece)
+{
+	TicTacToeBoard obj;
+	Piece outcome = obj.getPiece(0,0);
+	ASSERT_EQ(outcome, Invalid);	
+}
+
+TEST(TicTacToeBoardTest, getWinner)
+{
+	TicTacToeBoard obj;
+	Piece outcome = obj.getWinner();
+	ASSERT_EQ(outcome, Invalid);	
+}
